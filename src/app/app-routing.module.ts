6d3ae0673f87b6f6bbc102892e84c16d60aa25f8
Tiyasha Mallick <20051855@kiit.ac.in>
2023-11-02 17:03:41 +0530

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SongAddComponent } from './song-add/song-add.component';
-import { SongDeleteComponent } from './song-delete/song-delete.component';
 
-const routes: Routes = [
-  {
-    path:'song-add',
-    component: SongAddComponent
-  },
-  {
-    path:'song-delete',
-    component: SongDeleteComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
